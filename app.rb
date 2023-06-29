492fbfd6a7e89b29cd2c074d38b55373068a0ee1
oovillagran <oscar.villagran@hotmail.com>
2023-06-29 17:28:28 -0500

    Add main.rb

--- app.rb
+++ app.rb
@@ -5,69 +5,108 @@ require_relative 'rental'
 require_relative 'student'
 require_relative 'teacher'
 
-def list_of_books
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+    puts 'Welcome to Library App!'
+    puts
+  end
 
-end
+  def list_of_books
+    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    puts
+  end
+  
+  # def list_of_people
+  
+  # end
+  
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    person_type = gets.chomp.to_i
 
-def list_of_people
-
-end
-
-def create_person
-
-end
-
-def create_book
-
-end
-
-def create_rental
-
-end
-
-def list_of_rentals(person_id)
-
-end
-
-def display_menu
-  puts 'Please choose and option by enterin a number'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
-
-def main
-  loop do
-    display_menu
-    option = gets.chomp.to_i
-
-    case option
-    when 1 
-      list_of_books
+    case person_type
+    when 1
+      create_student
     when 2
-      list_of_people
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6 list_of_rentals
-      puts 'Enter person ID:'
-      person_id = gets.chomp.to_i
-      list_of_rentals(person_id)
-    when 7
-      puts 'Good bye!'
-      break
+      create_teacher
     else
       puts 'Invalid option. Please try again.'
     end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp.to_i
+    print 'Parent Permission (Y/N): '
+    parent_permission_input = gets.chomp.upcase
+
+    parent_permission = parent_permission_input == 'y' ? true : false
+
+    student = Student.new(age, name, parent_permission: parent_permission)
+    @people << student
+
+    puts 'Student created sucesfully'
     puts
   end
-end
 
-main if __FILE__ == $PROGRAM_NAME
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+
+    @books << Book.new(title, author)
+    puts 'Book created successfully'
+    puts
+  end
+  
+  # def create_rental
+  
+  # end
+  
+  # def list_of_rentals(person_id)
+  
+  # end
+  
+  def handle_menu(option)
+    options = {
+      '1' => method(:list_of_books),
+      # '2' => method(:list_all_people),
+      '3' => method(:create_person),
+      '4' => method(:create_book),
+      # '5' => method(:create_rental),
+      # '6' => method(:list_of_rentals),
+      '7' => proc {
+              puts 'Thank you for using this app. Good Bye!'
+              exit
+            }
+    }
+
+    if(options)[option]
+      options[option].call
+    else
+      puts 'Invalid option. Please try again.'
+    end
+  end
+
+  def main
+    puts 'Please choose and option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+
+    option = gets.chomp
+
+    handle_menu(option)
+    main
+  end
+end
--- main.rb
+++ main.rb
@@ -1,116 +1,12 @@
-require_relative 'person'
-require_relative 'classroom'
-require_relative 'book'
-require_relative 'student'
-require_relative 'rental'
-require_relative 'teacher'
+require_relative 'app'
 
-# Create instances and perform tests for each class
+app = App.new
+app.main
 
-# Person
-person = Person.new(22, 'maximilianus')
-# person_1 = Person.new(25, 'John')
-# person_2 = Person.new(30, 'Alice')
-puts 'Person:'
-puts "Name: #{person.correct_name}"
-puts "Age: #{person.age}"
-puts "Can use services? #{person.can_use_services?}"
-# puts 'Person 1:'
-# puts "Name: #{person_1.correct_name}"
-# puts "Age: #{person_1.age}"
-# puts "Can use services? #{person_1.can_use_services?}"
-# puts 'Person 2:'
-# puts "Name: #{person_2.correct_name}"
-# puts "Age: #{person_2.age}"
-# puts "Can use services? #{person_2.can_use_services?}"
-puts
+# def main
 
-# Decorator
-capitalized_person = CapitalizeDecorator.new(person)
-puts 'Capitalized Person:'
-puts "Correct name: #{capitalized_person.correct_name}"
-puts
 
-# TrimmerDecorator
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts 'Capitalized and Trimmed Person:'
-puts "Correct name: #{capitalized_trimmed_person.correct_name}"
-puts
 
-# Classroom
-classroom = Classroom.new('Class 101')
-puts 'Classroom:'
-puts "Label: #{classroom.label}"
-puts
-
-# Book
-book = Book.new('From Earth to the Moon', 'Julio Verne')
-# book_1 = Book.new('Leadership', 'Joyce Meyer')
-# book_2 = Book.new('Lion\'s Heart', 'Abel Dwen')
-puts 'Book:'
-puts "Title: #{book.title}"
-puts "Author: #{book.author}"
-# puts 'Book 1:'
-# puts "Title: #{book_1.title}"
-# puts "Author: #{book_1.author}"
-# puts 'Book 2:'
-# puts "Title: #{book_2.title}"
-# puts "Author: #{book_2.author}"
-puts
-
-# Student
-student = Student.new(16, classroom, 'Kilian')
-puts 'Student:'
-puts "Name: #{student.name}"
-puts "Age: #{student.age}"
-puts "Can use services? #{student.can_use_services?}"
-puts "Classroom: #{student.classroom.label}"
-puts "Play hooky: #{student.play_hooky}"
-puts "Student's Classroom: #{student.classroom.label}"
-puts 'Students in the Classroom:'
-classroom.students.each do |student_each|
-  puts "- #{student_each.name}"
-end
-puts
-
-# Rental
-rental = Rental.new('28-06-2023', person, book)
-puts 'Rental:'
-puts "Date: #{rental.date}"
-puts "Book title: #{rental.book.title}"
-puts "Person name: #{rental.person.correct_name}"
-puts
-
-# Adding rentals for person 1
-# person_1.add_rental(book_2, '2023-06-23')
-# person_1.add_rental(book_1, '2023-06-24')
-
-# Adding rentals for person2
-# person_2.add_rental(book, '2023-06-25')
-
-# Printing the rentals for each person
-puts "Rentals for #{person.name}:"
-person.rentals.each do |rental_item|
-  puts "#{rental_item.book.title} - #{rental_item.date}"
-end
-puts
-
-# puts "Rentals for #{person_1.name}:"
-# person_1.rentals.each do |rental_item|
-#   puts "#{rental_item.book.title} - #{rental_item.date}"
 # end
-# puts
 
-# puts "Rentals for #{person_2.name}:"
-# person_2.rentals.each do |rental_item|
-#   puts "#{rental_item.book.title} - #{rental_item.date}"
-# end
-# puts
-
-# Teacher
-teacher = Teacher.new(45, 'Economics', 'Mrs. Grace')
-puts 'Teacher:'
-puts "Name: #{teacher.correct_name}"
-puts "Age: #{teacher.age}"
-puts "Can use services? #{teacher.can_use_services?}"
-puts
+# main if __FILE__ == $PROGRAM_NAME
