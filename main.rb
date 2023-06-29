cc9794aaab584a15f80e502d94b9006eb2f2f363
oovillagran <oscar.villagran@hotmail.com>
2023-06-29 11:36:40 -0500

    Add changes to meet CR requirements

--- main.rb
+++ main.rb
@@ -9,20 +9,20 @@ require_relative 'teacher'
 
 # Person
 person = Person.new(22, 'maximilianus')
-person_1 = Person.new(25, 'John')
-person_2 = Person.new(30, 'Alice')
+# person_1 = Person.new(25, 'John')
+# person_2 = Person.new(30, 'Alice')
 puts 'Person:'
 puts "Name: #{person.correct_name}"
 puts "Age: #{person.age}"
 puts "Can use services? #{person.can_use_services?}"
-puts 'Person 1:'
-puts "Name: #{person_1.correct_name}"
-puts "Age: #{person_1.age}"
-puts "Can use services? #{person_1.can_use_services?}"
-puts 'Person 2:'
-puts "Name: #{person_2.correct_name}"
-puts "Age: #{person_2.age}"
-puts "Can use services? #{person_2.can_use_services?}"
+# puts 'Person 1:'
+# puts "Name: #{person_1.correct_name}"
+# puts "Age: #{person_1.age}"
+# puts "Can use services? #{person_1.can_use_services?}"
+# puts 'Person 2:'
+# puts "Name: #{person_2.correct_name}"
+# puts "Age: #{person_2.age}"
+# puts "Can use services? #{person_2.can_use_services?}"
 puts
 
 # Decorator
@@ -45,33 +45,31 @@ puts
 
 # Book
 book = Book.new('From Earth to the Moon', 'Julio Verne')
-book_1 = Book.new('Leadership', 'Joyce Meyer')
-book_2 = Book.new('Lion\'s Heart', 'Abel Dwen')
+# book_1 = Book.new('Leadership', 'Joyce Meyer')
+# book_2 = Book.new('Lion\'s Heart', 'Abel Dwen')
 puts 'Book:'
 puts "Title: #{book.title}"
 puts "Author: #{book.author}"
-puts 'Book 1:'
-puts "Title: #{book_1.title}"
-puts "Author: #{book_1.author}"
-puts 'Book 2:'
-puts "Title: #{book_2.title}"
-puts "Author: #{book_2.author}"
+# puts 'Book 1:'
+# puts "Title: #{book_1.title}"
+# puts "Author: #{book_1.author}"
+# puts 'Book 2:'
+# puts "Title: #{book_2.title}"
+# puts "Author: #{book_2.author}"
 puts
 
 # Student
 student = Student.new(16, classroom, 'Kilian')
-student_1 = Student.new(12, classroom, 'Amy')
-student_2 = Student.new(15, classroom, 'Rachel')
 puts 'Student:'
-puts "Name: #{student.correct_name}"
+puts "Name: #{student.name}"
 puts "Age: #{student.age}"
 puts "Can use services? #{student.can_use_services?}"
 puts "Classroom: #{student.classroom.label}"
 puts "Play hooky: #{student.play_hooky}"
 puts "Student's Classroom: #{student.classroom.label}"
 puts 'Students in the Classroom:'
-classroom.students.each do |student|
-puts "- #{student.name}"
+classroom.students.each do |student_each|
+  puts "- #{student_each.name}"
 end
 puts
 
@@ -84,30 +82,30 @@ puts "Person name: #{rental.person.correct_name}"
 puts
 
 # Adding rentals for person 1
-person_1.add_rental(book_2, '2023-06-23')
-person_1.add_rental(book_1, '2023-06-24')
+# person_1.add_rental(book_2, '2023-06-23')
+# person_1.add_rental(book_1, '2023-06-24')
 
 # Adding rentals for person2
-person_2.add_rental(book, '2023-06-25')
+# person_2.add_rental(book, '2023-06-25')
 
 # Printing the rentals for each person
 puts "Rentals for #{person.name}:"
-person.rentals.each do |rental|
-  puts "#{rental.book.title} - #{rental.date}"
+person.rentals.each do |rental_item|
+  puts "#{rental_item.book.title} - #{rental_item.date}"
 end
 puts
 
-puts "Rentals for #{person_1.name}:"
-person_1.rentals.each do |rental|
-  puts "#{rental.book.title} - #{rental.date}"
-end
-puts
+# puts "Rentals for #{person_1.name}:"
+# person_1.rentals.each do |rental_item|
+#   puts "#{rental_item.book.title} - #{rental_item.date}"
+# end
+# puts
 
-puts "Rentals for #{person_2.name}:"
-person_2.rentals.each do |rental|
-  puts "#{rental.book.title} - #{rental.date}"
-end
-puts
+# puts "Rentals for #{person_2.name}:"
+# person_2.rentals.each do |rental_item|
+#   puts "#{rental_item.book.title} - #{rental_item.date}"
+# end
+# puts
 
 # Teacher
 teacher = Teacher.new(45, 'Economics', 'Mrs. Grace')
